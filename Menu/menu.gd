4c541b06d60ae698d741f59325572715612eafe2
StefaniaKotsi <stefaniakwtsi@gmail.com>
2024-10-18 02:10:27 +0200

    Main Menu graphics added
    Options error still there!
    You can polish your drawing if you want, but don't move the rest

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -1,6 +1,8 @@
 extends Control
+@onready var options: Button = $MarginContainer/VBoxContainer/Options as Button
 
 func _on_play_pressed() -> void:
+	print("Playing!")
 	get_tree().change_scene_to_file("res://game.tscn")
 
 
@@ -9,12 +11,12 @@ func _on_controls_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
+	print("Options")
 	get_tree().change_scene_to_file("res://options_menu.tscn")
 
 
 func _on_credits_pressed() -> void:
 	pass # Replace with function body.
 
-
-func _on_quit_pressed() -> void:
+func _on_exit_pressed() -> void:
 	get_tree().quit()
