abd104afd1953f2ce8e34bd8fdbac66695c21d3c
StefaniaKotsi <stefaniakwtsi@gmail.com>
2024-10-18 16:25:56 +0200

    Fixed error, added graphics

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -7,16 +7,17 @@ func _on_play_pressed() -> void:
 
 
 func _on_controls_pressed() -> void:
+	print("Getting controls")
 	pass # Replace with function body.
-
 
 func _on_options_pressed() -> void:
 	print("Options")
-	get_tree().change_scene_to_file("res://options_menu.tscn")
-
+	get_tree().change_scene_to_file("res://Menu/options_menu.tscn")
 
 func _on_credits_pressed() -> void:
+	print("Getting credits")
 	pass # Replace with function body.
 
 func _on_exit_pressed() -> void:
+	print("Exiting...")
 	get_tree().quit()
--- Menu/options_menu.gd
+++ Menu/options_menu.gd
@@ -1,4 +1,5 @@
 extends Control
 
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://menu.tscn")
+	print("Back to main menu")
+	get_tree().change_scene_to_file("res://Menu/menu.tscn")
