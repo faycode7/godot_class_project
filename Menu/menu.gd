07c7ae896380d7f3e40395b01b83de98dae6dbf3
StefaniaKotsi <stefaniakwtsi@gmail.com>
2024-10-29 17:50:38 +0200

    added assets to interact in 1st scene
    I have the dialogue in my notepad (different in 1st and 2nd interaction) )but haven't implemented it yet

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -1,23 +1,23 @@
 extends Control
 @onready var options: Button = $MarginContainer/VBoxContainer/Options as Button
 
-func _on_play_pressed():
+func _on_play_pressed() -> void:
 	print("Playing!")
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://1st scene/1_st_scene.tscn")
 
 
-func _on_controls_pressed():
+func _on_controls_pressed() -> void:
 	print("Getting controls")
 	pass # Replace with function body.
 
-func _on_options_pressed():
+func _on_options_pressed() -> void:
 	print("Options")
 	get_tree().change_scene_to_file("res://Menu/options_menu.tscn")
 
-func _on_credits_pressed():
+func _on_credits_pressed() -> void:
 	print("Getting credits")
 	pass # Replace with function body.
 
-func _on_exit_pressed():
+func _on_exit_pressed() -> void:
 	print("Exiting...")
 	get_tree().quit()
