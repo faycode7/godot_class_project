3f26da52dd235580943c674f2ddadadeec5e8c64
faycode7 <fayvalari@gmail.com>
2024-11-02 20:52:09 +0200

    added bones to enemy system

--- Bullet Hell Implimentation/bossies/enemy_state_machine.gd
+++ Bullet Hell Implimentation/bossies/enemy_state_machine.gd
@@ -1,3 +1,30 @@
 extends Node2D
 @onready var sprite = $Sprite
 @onready var script_holder = $Node
+var health = 0
+var attack_counter = 0
+var enemy_variables
+@onready var attacks = $attacks
+
+func _ready():
+	get_attack_available()
+	enemy_start()
+	attack()
+
+func get_attack_available():
+	if script_holder.get_child_count() > 0:
+		enemy_variables = script_holder.get_child(0)
+func sprite_setter(enemy_sprite):
+	sprite.texture =load(enemy_sprite) 
+func health_setter(enemy_health):
+	health = enemy_health
+func attack():
+	attack_counter = randi_range(0,enemy_variables.attack_list.size()-1)
+	var attack = load(enemy_variables.attack_list[attack_counter]).instantiate()
+	attacks.add_child(attack)
+
+func enemy_start():
+	sprite_setter(enemy_variables.sprite)
+	health_setter(enemy_variables.health)
+	#get_attack_pattern(enemy_variables.attack_list)
+	
---
+++ enemy_attack_patterns/test_enemy_pattern.gd
@@ -0,0 +1,6 @@
+extends Node
+var health = 300
+var sprite = "res://Art/Backgrounds/house_ex.png"
+var attack_list = [
+	"res://Bullet Hell Implimentation/enemies/dollop_spawner.tscn"
+]
