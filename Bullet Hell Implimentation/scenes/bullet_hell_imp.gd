cc3d853d6cb43471e8512d4eb46282a93c19ce29
faycode7 <fayvalari@gmail.com>
2024-11-04 17:58:14 +0200

    still working on fighting BONES

--- Bullet Hell Implimentation/scenes/bullet_hell_imp.gd
+++ Bullet Hell Implimentation/scenes/bullet_hell_imp.gd
@@ -1,15 +1,14 @@
 extends Node2D
 @onready var healthbar = $GUI/healthbar
 @onready var health_nums = $GUI/Label
-@onready var enemy = $EnemyHolder
+@onready var enemy = $enemy_state_machine
+@onready var player = $Player_fox
 var death_scene = "res://Bullet Hell Implimentation/scenes/game_over_screen.tscn"
 var list_of_levels = ["res://Bullet Hell Implimentation/enemies/dollop_spawner.tscn"]
-@onready var player = $Player_fox
-
 
 func _ready():
 	update_hp()
-	start_fight()
+	#start_fight()
 	healthbar.max_value = Glob.max_health
 
 func update_hp():
@@ -20,12 +19,17 @@ func spawn_death():
 	var death = load(death_scene).instantiate()
 	add_child(death)
 	
-func start_fight():
-	if (enemy.get_child_count()>0):
-		enemy.get_child(0).queue_free()
-	var level = load(list_of_levels[Glob.current_level]).instantiate()
-	enemy.add_child(level)
+#func start_fight():
+	#if (enemy.get_child_count()>0):
+		#enemy.get_child(0).queue_free()
+	#var level = load(list_of_levels[Glob.current_level]).instantiate()
+	#enemy.add_child(level)
 
 func setPosition(setPos):
 	player.position = setPos 
+
+func apply_damage(damage):
+	enemy.health -= damage
+	
+	
 	
