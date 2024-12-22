3adf5a78b1c1f27027400cb30edbdd1db5c6a24d
Stef <stefaniakwtsi@gmail.com>
2024-12-22 21:16:13 +0200

    deleted the things I had done

--- Inventory System/item.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-class_name Item
-
-var stackable: bool
-var item_type: String #weapon or food
-var quantity: int 
-var max_quantity: int
-var price: int
--- Menu/menu_scripts/controls.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-func _on_back_pressed() -> void:
-	print("Back to main menu")
-	get_tree().change_scene_to_file("res://Menu//menu_scenes/menu.tscn")
